bd27563bbc5e075b52639231a637b842dbe78c85
Gramone33 <60742051+Gramone33@users.noreply.github.com>
2020-05-28 18:12:07 +0200

    creation of catalogAPI

---
+++ src/Services/Catalog/Catalog.API/Models/CatalogRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Models
+{
+    public class CatalogRepository : ICatalogRepository
+    {
+        private SqlConnection _db;
+        public CatalogRepository(string connectionString) =>
+            _db = new SqlConnection(connectionString);
+        
+        public void Dispose() =>
+            _db.Dispose();
+
+        public CatalogItem GetItemById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CatalogItem> GetItems(int pageSize, int pageNum)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
